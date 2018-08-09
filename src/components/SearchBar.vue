29d95ae46a34d7ceb203c8917d5a11c39ea9d36e
Melissa Moreira <xmelissamoreirax@gmail.com>
2018-08-08 21:33:08 -0300

    Initial Commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="searchbar">
+   <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {  
+    onInput: (event) => {
+      console.log(event.target.value);
+    }
+  }
+};
+</script>
+
+
+<style lang="stylus" scoped>
+
+</style>
